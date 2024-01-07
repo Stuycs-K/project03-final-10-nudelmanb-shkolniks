fe59db80e60461cc3e3984541a2fa573170e220f
Benjamin Nudelman <bnudelman40@stuy.edu>
2024-01-06 22:22:57 -0500

    created functional ssh to lab machine 01, yet to allow condition to bypass fingerprint, tested sample code on the lab machine once ssh is ran, ssh is run in non-interactive mode

---
+++ lab.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("this is done on the lab machine\n");
+    return 0;
+}
--- main.c
+++ main.c
@@ -13,7 +13,7 @@ information through the socket. This file should be run on marge other simpson-n
 */
 int connect_labs(char* lab, int machine_number, char* user){
     char* IP = malloc(14);
-    strcpy(IP, "149.90.");
+    strcpy(IP, "149.89.");
     strcat(IP, lab);
     strcat(IP, ".1");
     if (machine_number < 10){
@@ -23,30 +23,25 @@ int connect_labs(char* lab, int machine_number, char* user){
     sprintf(machine_string, "%d", machine_number);
     strcat(IP, machine_string);
 
-    // char* password = malloc(BUFFER_SIZE)
-    // printf("Please enter your password")
-
     char* args[16];
     args[0] = "ssh";
 
-    char* sshprompt = malloc(124);
+    char sshprompt[124];
     strcpy(sshprompt, user);
     strcat(sshprompt, "@");
     strcat(sshprompt, IP);
 
     args[1] = sshprompt;
-    args[2] = NULL;
+    args[2] = "cd Desktop/project03-final-10-nudelmanb-shkolniks;./lab";
+    args[3] = NULL;
 
     printf("%s", sshprompt);
-    err();
     execvp(args[0], args);
-    err();
     
-    printf("got here");
     return 0; //0 for functional, yet to add conditions
 }
 
 
 int main(int argc, char *argv[]){
-    connect_labs("150", 15, "bnudelman40");
+    connect_labs("161", 1, "bnudelman40");
 }
